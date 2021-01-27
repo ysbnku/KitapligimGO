f0ad4fb2c3b3e5afa75be485d0b0242be3bd3639
Yavuz Bitmez <yavuzbitmez.nku@gmail.com>
2021-01-27 15:38:28 +0300

    Odakitap revize

--- controller/odakitap.go
+++ controller/odakitap.go
@@ -17,27 +17,25 @@ func Odakitap(books *model.Books, s string) {
 	if err != nil {
 		log.Println(err)
 	} else {
-		bow.Find(".plist-item").Each(func(i int, item *goquery.Selection) {
-			title := "Deneme"
+		bow.Find(".plist-item").Each(func(index int, item *goquery.Selection) {
+			title := item.Find(".plist-info h2").Text()
 			author := item.Find(".l-owner h3").Text()
 			pub := item.Find(".l-owner h4").Text()
 			img, _ := item.Find(".plist-image-wrapper img").Attr("src")
-			price := "22"
+			price := item.Find(".new-price").Text()
 			website, _ := item.Find(".plist-image-wrapper a").Attr("href")
 
-			if title != "" && price != "" {
-				p := model.Book{
-					Title:      title,
-					Author:     author,
-					Publisher:  pub,
-					Img:        "https://www.odakitap.com" + img,
-					Price:      price,
-					PriceFloat: 0.0,
-					WebSite:    "https://www.odakitap.com" + website,
-					Resource:   "Odakitap",
-				}
-				model.Add(&p, books)
+			p := model.Book{
+				Title:      title,
+				Author:     author,
+				Publisher:  pub,
+				Img:        "https://www.odakitap.com" + img,
+				Price:      price,
+				PriceFloat: 0.0,
+				WebSite:    "https://www.odakitap.com" + website,
+				Resource:   "Odakitap",
 			}
+			model.Add(&p, books)
 		})
 	}
 }
