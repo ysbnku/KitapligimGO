d4bf30d0a389002199b9d01e3dbe0970274c9a35
Yavuz Bitmez <yavuzbitmez.nku@gmail.com>
2021-01-27 16:57:05 +0300

    Odakitap revize

--- controller/idefix.go
+++ controller/idefix.go
@@ -32,7 +32,7 @@ func Idefix(books *model.Books, s string) {
 			title := item.Find(".box-title a").Text()
 			author := item.Find(".pName a").Text()
 			pub := item.Find(".manufacturerName a").Text()
-			img, _ := item.Find("img").Attr("src")
+			img, _ := "aaa"
 			price := item.Find("#prices").Text()
 			website, _ := item.Find(".box-title a").Attr("href")
 
