fbc3d72b9ed1c5b55d58c65f40dbdc091f80cb3d
Yavuz Bitmez <yavuzbitmez.nku@gmail.com>
2021-04-10 18:10:31 +0300

    Test

--- controller/search.go
+++ controller/search.go
@@ -10,8 +10,8 @@ var wg sync.WaitGroup
 func Search(books *model.Books, s string) *model.Books {
 	wg.Add(4)
 	go Idefix(books, s)
-	go Odakitap(books, s)
-	// go Pandora(books, s)
+	// go Odakitap(books, s)
+	go Pandora(books, s)
 	go Sozcu(books, s)
 	go Dr(books, s)
 	// go KitapYurdu(books, s)
